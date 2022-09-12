4b46ec59cb228a2587ab6c7dc17ab65ee004c699
YashG0728 <yashgharde07@gmail.com>
2022-09-12 23:22:16 +0530

    Added : First Commit

---
+++ Stack.java
@@ -0,0 +1,31 @@
+package UC1;
+
+public class Stack<K> {
+
+    public INode<K> head;
+    public INode<K> tail;
+
+    public Stack() {
+        this.head = null;
+        this.tail = null;
+    }
+
+    public void push(INode<K> newNode) {
+        if (this.tail == null) {
+            this.tail = newNode;
+        }
+        if (this.head == null) {
+            this.head = newNode;
+        } else {
+            INode<K> tempNode = this.head;
+            this.head = newNode;
+            this.head.setNext(tempNode);
+        }
+    }
+
+
+    public void printMyNode() {
+        System.out.println("My Nodes : " + head);
+    }
+}
+
